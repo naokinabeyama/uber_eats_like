a24bf1001006028b0278867a9df75080ea35d191
vagrant <gachapin5689@gmail.com>
2021-03-23 06:54:46 +0000

    20210323

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  # あえてロード時間を１秒作りローディング状態を見る
+  before_action :fake_load
+
+  def fake_load
+    sleep(1)
+  end
 end
--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,13 +1,20 @@
 // コンポーネント = 任意の入力を受け取り、画面上に表示すべきものを記述するReactを返す
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { fetchFoods } from '../apis/foods';
 
-export const Foods = ({ match }) => {
+export const Foods = ({
+  match
+}) => {
+  useEffect(() => {
+    fetchFoods(match.params.restaurantsId)
+      .then((data) =>
+        console.log(data)
+      )
+  }, [])
+
   return (
     <Fragment>
       フード一覧
-      <p>
-        restaurantsIdは {match.params.restaurantsId} です
-      </p>
     </Fragment>
   )
 }
