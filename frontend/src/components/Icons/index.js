5aefdfc92be749e0e24d92846a53ad01cc79c286
vagrant <gachapin5689@gmail.com>
2021-03-29 08:17:11 +0000

    20210329

---
+++ frontend/src/components/Icons/index.js
@@ -0,0 +1,3 @@
+// Material UIから取り込んだアイコン一覧を定義
+export { default as LocalMallIcon } from '@material-ui/icons/LocalMall';
+export { default as QueryBuilderIcon } from '@material-ui/icons/QueryBuilder';
--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,5 +1,10 @@
 // コンポーネント = 任意の入力を受け取り、画面上に表示すべきものを記述するReactを返す
 import React, { Fragment, useEffect, useReducer } from 'react';
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import { LocalMallIcon } from '../components/Icons';
+import { FoodWrapper } from '../components/FoodWrapper';
+import Skeleton from '@material-ui/lab/Skeleton';
 import {
   // initialStateという名前のmoduleをfoodsInitialStateという名前にしてimportしている
   initialState as foodsInitialState,
@@ -7,7 +12,41 @@ import {
   foodsReducer,
 } from '../reducers/foods';
 import { fetchFoods } from '../apis/foods';
+import MainLogo from '../images/logo.png';
+import FoodImage from '../images/food-image.jpg';
 import { REQUEST_STATE } from '../constants';
+import { COLORS } from '../style_constants';
+
+
+const HeaderWrapper = styled.div`
+  display:flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+  `;
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+  `;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+  `;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+  `;
+
+const FoodsList = sytled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+  `;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+  `;
+
 
 export const Foods = ({
   match
@@ -29,20 +68,42 @@ export const Foods = ({
 
   return (
     <Fragment>
-      {
-        foodsState.fetchState === REQUEST_STATE.LOADING ?
-          <Fragment>
-            <p>
-              ロード中...
-            </p>
-          </Fragment>
-          :
-          foodsState.foodsList.map(food =>
-            <div key={food.id}>
-              {food.name}
-            </div>
-          )
-      }
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="/orders">
+            <ColoredBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodsList>
+        {
+          foodsState.fetchState === REQUEST_STATE.LOADING ?
+            <Fragment>
+              {
+                // 12個のSkeletonコンポーネントを並べている([0~11])
+                [...Array(12).keys()].map(i =>
+                  <ItemWrapper key={i}>
+                    <Skeleton key={i} variant="react" width={450} height={180} />
+                  </ItemWrapper>
+                )
+              }
+            </Fragment>
+            :
+            // APIから取得したフード一覧が入る
+            foodsState.foodsList.map(food =>
+              <ItemWrapper key={food.id}>
+                <FoodWrapper
+                  food={food}
+                  onClickFoodWrapper={(food) => console.log(food)}
+                  imageUrl={FoodImage}
+                />
+              </ItemWrapper>
+            )
+        }
+      </FoodsList>
     </Fragment>
   )
 }
