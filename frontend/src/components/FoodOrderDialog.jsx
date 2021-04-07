6d911a3286216b6eab92ff8e959cb1a9ce838bc2
vagrant <gachapin5689@gmail.com>
2021-04-07 06:12:17 +0000

    20210407

--- frontend/src/components/FoodOrderDialog.jsx
+++ frontend/src/components/FoodOrderDialog.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { DialogContent, Dialog, DialogTitle, DialogActions } from '@material-ui/core';
 import styled from 'styled-components';
 import { SubText } from './StyledText';
+import { CountUpButton } from './Button/CountUpButton';
+import { CountDownButton } from './Button/CountDownButton';
+import { OrderButton } from './Button/OrderButton';
 import OrderHeaderImage from '../images/order-header.png';
 
 const OrderHeader = styled.img`
@@ -14,10 +17,39 @@ const DescriptionWrapper = styled.div`
   height: 50px;
 `;
 
+const CounntersWrapper = styled.div`
+  margin-right: auto;
+  display: flex;
+  padding: 0 16px;
+`;
+
+const CountItem = styled.div`
+  margin: 0 8px;
+`;
+
+const CountNum = styled.div`
+  padding-top: 10px;
+`;
+
+const OrderTextWrapper = styled.div`
+  display: flex;
+`;
+
+const OrderButtonTextWrapper = styled.div`
+  width: 300px;
+`;
+
+const PriceWrapper = styled.div`
+  padding-top: 4px;
+`;
+
 export const FoodOrderDialog = ({
   food,
   isOpen,
   onClose,
+  onClickCountUp,
+  onClickCountDown,
+  onClickOrder,
 }) => {
   return (
     <Dialog
@@ -36,7 +68,37 @@ export const FoodOrderDialog = ({
         </DescriptionWrapper>
       </DialogContent>
       <DialogActions>
-        {/* 数量を操作するアクションを入れる予定 */}
+        <CountersWrapper>
+          <CountItem>
+            <CountDownButton
+              onClick={() => onClickCountDown()}
+              // 数量が１以下ならカウントダウンさせない
+              isDisabled={countNumber <= 1}
+            />
+          </CountItem>
+          <CountItem>
+            <CountNum>
+              {countNumber}
+            </CountNum>
+          </CountItem>
+          <CountItem>
+            <CountUpButton
+              onClick={() => onClickCountUp()}
+              // 数量が９以上ならカウントアップさせない
+              isDisabled={countNumber >= 9}
+            />
+          </CountItem>
+        </CountersWrapper>
+        <OrderButton onClick={() => onClickOrder()}>
+          <OrderTextWrapper>
+            <OrderButtonTextWrapper>
+              {`${countNumber}点を注文に追加`}
+            </OrderButtonTextWrapper>
+            <PriceWrapper>
+              {`¥${countNumber * food.price}`}
+            </PriceWrapper>
+          </OrderTextWrapper>
+        </OrderButton>
       </DialogActions>
     </Dialog>
   )
--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -48,6 +48,10 @@ const ItemWrapper = styled.div`
   margin: 16px;
   `;
 
+const submitOrder = () => {
+  console.log('登録ボタンが押された')
+}
+
 
 export const Foods = ({
   match
@@ -125,9 +129,22 @@ export const Foods = ({
         <FoodOrderDialog
           food={state.selectedFood}
           isOpen={state.isOpenOrderDialog}
+          countNumber={state.selectedFoodCount}
+          onClickCountUp={() => setState({
+            ...state,
+            selectedFoodCount: state.selectedFoodCount + 1,
+          })}
+          onClickCountDown={() => setState({
+            ...state,
+            selectedFoodCount: state.selectedFoodCount - 1,
+          })}
+          onClickOrder={() => submitOrder()}
+          // モーダルを閉じるときは全てのstateを初期化する
           onClose={() => setState({
             ...state,
             isOpenOrderDialog: false,
+            selectedFood: null,
+            selectedFoodCount: 1,
           })}
         />
       }
